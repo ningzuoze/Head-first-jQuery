144a57d35bb1a868d8996916aeb85aafbf97fba0
ningzuoze <1911463996@qq.com>
2018-03-10 18:47:31 +0800

    第三章

--- 选择器和方法/script/my_scripts.js
+++ 选择器和方法/script/my_scripts.js
@@ -24,16 +24,35 @@ $(document).ready(function(){
     function checkForcode(){
         var discount;
         if ($.contains(this,document.getElementById("has_discount"))){//判断绑定事件的元素是否为随机加的span元素
-            var my_num=getRandom(5);//查找到的话产生一个1~5的随机数
-            discount="<p>你的折扣为："+my_num+"折</p>";//并输出随机的折扣
+            var my_num=getRandom(100);//查找到的话产生一个1~100的随机数
+            discount="<p>你的折扣为码为："+my_num+"</p>";//并输出随机的折扣
         } else {
             discount="<p>不好意思你没抽中折扣！</p>"//否则提示没抽中
         }
-        $(this).append(discount);//增加到事件元素的后面
+        $("#result").append(discount);//增加到事件元素的后面的p标签中
         $(".guess_box").each(function(){
-            $(this).unbind('click');
-        })//去除其他的和所点击元素的click事件
+            if($.contains(this,document.getElementById("has_discount")))//判断是否为猜中的div
+            {
+                $(this).addClass("discount");//是的话改变样式
+            }else{
+                $(this).addClass("no_discount");//不是的话增加另一种样式
+            }
+            $(this).unbind('click');//去除其他的和所点击元素的click事件
+        });//遍历所有class为.guess_box的元素
     }
     //end checkForcode function
+
+        $(".guess_box").hover(
+        function(){
+            //移入事件
+            $(this).addClass("my_hover");
+        },
+        function(){
+            //移出事件
+            $(this).removeClass("my_hover");
+        }
+    )//为class为guess_box的元素添加样式
 });
+
+
 
