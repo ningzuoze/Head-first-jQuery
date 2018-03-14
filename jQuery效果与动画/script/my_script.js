9f93437a5c3339eaad6021c014bb1071f5a9e6aa
ningzuoze <1911463996@qq.com>
2018-03-14 21:39:47 +0800

    第五章

--- jQuery效果与动画/script/my_script.js
+++ jQuery效果与动画/script/my_script.js
@@ -1,5 +1,8 @@
 $(document).ready(function(){
         var headclix=0,eyeclix=0,noseclix=0,mouthclix=0;
+        lightning_one(4000);
+        lightning_two(5000);
+        lightning_three(7000);
 
         function face(parts,partsclix){
                 $("#"+parts).click(function(){
@@ -10,16 +13,20 @@ $(document).ready(function(){
                         }
                 })
         }//end face function
-        lightning(4000);
-        function lightning(t){
-                $("#container #lightning1").fadeIn(250).fadeOut(250);
-                setTimeout("lightning(t)",t);
-        }
-
-        function lightning_one(){
-                $("#container #lightning1").fadeIn(250).fadeOut(250);
-                setTimeout("lightning_one()",4000);
-        };
         
+        function lightning_one(t){
+                $("#container #lightning1").fadeIn(250).fadeOut(250);
+                setTimeout("lightning_one()",t);
+        }
+        function lightning_two(t){
+                $("#container #lightning2").fadeIn(250).fadeOut(250);
+                setTimeout("lightning_two()",t);
+        }
+        function lightning_three(t){
+                $("#container #lightning_three").fadeIn(250).fadeOut(250);
+                setTimeout("lightning_three()",t);
+        }
+        
+
 
 })
