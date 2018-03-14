0240f7c77e09e69411fdd935101a92dc556b55cb
ningzuoze <1911463996@qq.com>
2018-03-14 23:38:57 +0800

    第五章

--- jQuery效果与动画/script/my_script.js
+++ jQuery效果与动画/script/my_script.js
@@ -1,62 +1,30 @@
 $(document).ready(function(){
-        var headclix=0,eyeclix=0,noseclix=0,mouthclix=0;
-        lightning_one(4000);
-        lightning_two(5000);
-        lightning_three(7000);
 
-        $("#head").click(function(){
-                if(headclix<9){
-                        $(this).animate({left:"-=367px"},500);
-                        headclix+=1;
-                }else{
-                        $(this).animate({left:"0px"},500);
-                        headclix=0;
-                }
-        })
-        $("#eyes").click(function(){
-                if(eyeclix<9){
-                        $(this).animate({left:"-=367px"},500);
-                        eyeclix+=1;
-                }else{
-                        $(this).animate({left:"0px"},500);
-                        eyeclix=0;
-                }
-        })
-        
-        $("#nose").click(function(){
-                if(noseclix<9){
-                        $(this).animate({left:"-=367px"},500);
-                        noseclix+=1;
-                }else{
-                        $(this).animate({left:"0px"},500);
-                        noseclix=0;
-                }
-        })
-        
-        $("#mouth").click(function(){
-                if(mouthclix<9){
-                        $(this).animate({left:"-=367px"},500);
-                        mouthclix+=1;
-                }else{
-                        $(this).animate({left:"0px"},500);
-                        mouthclix=0;
-                }
-        })
-        
-        
-        function lightning_one(t){
-                $("#container #lightning1").fadeIn(250).fadeOut(250);
-                setTimeout("lightning_one()",t);
-        }
-        function lightning_two(t){
-                $("#container #lightning2").fadeIn(250).fadeOut(250);
-                setTimeout("lightning_two()",t);
-        }
-        function lightning_three(t){
-                $("#container #lightning_three").fadeIn(250).fadeOut(250);
-                setTimeout("lightning_three()",t);
-        }
+        face("head");
+        face("eyes");
+        face("nose");
+        face("mouth");
+        setInterval("lightning('lightning1')",4300);
+        setInterval("lightning('lightning2')",5000);
+        setInterval("lightning('lightning3')",7000);
         
 
 
+        function face(str){
+                var clix=0;//设定几面的值
+                $("#"+str).click(function(){//获取点击元素
+                        if(clix<9){//判断是否超过设定的面数
+                                $(this).animate({left:"-=367px"},500);//把图片想左移动
+                                clix+=1;//面数加1
+                        }else{
+                                $(this).animate({left:"0px"},500);//最后一面时调到第一面
+                                clix=0;//初始化面
+                        }
+                })
+        }//end face function
 })
+
+               
+        function lightning(str){
+                $("#"+str).fadeIn(250).fadeOut(250);//获取闪电隐藏和显示
+        }//end lightning function
