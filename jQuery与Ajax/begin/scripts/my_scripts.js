ffbb67248be02913f96610ce7da36865abf1efb0
ningzuoze <1911463996@qq.com>
2018-03-19 23:18:46 +0800

    第八章

---
+++ jQuery与Ajax/begin/scripts/my_scripts.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+    getTime();
+    function getTime(){
+        var a_p="";
+        var d=new Date;
+        var curr_hour=d.getHours();
+        (curr_hour<12)?a_p="AM":a_p="PM";
+        (curr_hour==0)?curr_hour=12:curr_hour=curr_hour;
+        (curr_hour>12)?curr_hour=curr_hour-12:curr_hour=curr_hour;
+        var curr_min=d.getMinutes().toString();
+        var curr_sec=d.getSeconds().toString();
+        if(curr_min.length==1){curr_min="0"+curr_min};
+        if(curr_sec.length==1){curr_sec="0"+curr_sec};
+        $('#updateTime').html(curr_hour+":"+curr_min+":"+curr_sec+" "+a_p);
+    }//返回时间函数
+    $("#main ul").idTabs("male"); 
+})
